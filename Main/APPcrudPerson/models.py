edcab71f7f7d9bc685f6486d7e879b8bf3be4999
“CipollaLucas” <lucascipolla1993@gmail.com>
2024-11-02 13:02:49 -0300

    Puesta en marcha.

---
+++ Main/APPcrudPerson/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Persona(models.Model):
+    id = models.AutoField(primary_key=True)   
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    Hobbie = models.CharField(max_length=100)
+
+    TIPO_DOCUMENTO = [
+        ('DNI', 'DNI'),
+        ('Pasaporte', 'Pasaporte'),
+    ]
+    tipo_documento = models.CharField(max_length=10, choices=TIPO_DOCUMENTO, default='DNI', unique=True)
+    numero_documento = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.apellido}, {self.nombre}'
